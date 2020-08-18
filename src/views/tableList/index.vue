e4e5ff19065656597736568fb9435570496a7a0c
serena_xu1 <serena_xu1@epam.com>
2020-08-18 18:51:30 +0800

    init

--- src/views/tableList/index.vue
+++ src/views/tableList/index.vue
@@ -1,13 +1,35 @@
 <template>
   <div>
     <a-button type="primary">tableList</a-button>
-    <div class=""></div>
+    <a-skeleton :loading="loading">
+      <div>
+        <TimeLine></TimeLine>
+      </div>
+    </a-skeleton>
   </div>
 </template>
 <script>
-import { Button } from "ant-design-vue";
+import { Button, Skeleton } from "ant-design-vue";
+import TimeLine from "@/components/TimeLine";
 export default {
   name: "tableList",
-  components: { AButton: Button },
+  components: {
+    AButton: Button,
+    TimeLine,
+    ASkeleton: Skeleton,
+  },
+  data() {
+    return {
+      loading: true,
+    };
+  },
+  mounted() {
+    this.$nextTick(() => {
+      // import TimeLine from "@/components/TimeLine";
+      setTimeout(() => {
+        this.loading = false;
+      }, 1000);
+    });
+  },
 };
 </script>
