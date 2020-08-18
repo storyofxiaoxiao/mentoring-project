f895a8723147178ff1e411870aa591c13161157a
serena_xu1 <serena_xu1@epam.com>
2020-08-18 10:34:04 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Layout from '@/views/layout';
+
+Vue.use(VueRouter);
+
+export const constantRouterMap = [{
+  path: '/',
+  redirect: '/home',
+}];
+export const baseRouterMap = [{
+  path: "/",
+  name: 'home',
+  component: Layout,
+  meta: {
+    icon: 'home',
+  },
+  children: [{
+    path: 'home',
+    component: () => import('@/views/home/index'),
+    name: 'home',
+    hidden: false,
+  }]
+}, ]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: constantRouterMap.concat(baseRouterMap),
+});
+
+export default router;
---
+++ src/views/home/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>123123123</div>
+</template>
+<script>
+export default {
+  name: "home",
+};
+</script>
